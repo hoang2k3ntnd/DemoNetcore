e352c6d29f40aaa8c8002d0c265947a080fa4258
hoang2k3ntnd <phama3071@gmail.com>
2023-02-23 17:47:58 +0700

    first commit

---
+++ Script1.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+< !DOCTYPE html >
+    <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+                <meta http-equiv="X-UA-compatible" content="IE-edge">
+                    <meta name="viewport" content="with=device=width,initial-scale=1.0">
+                        <title>document</title>
+                    </head>
+                    <body>
+                        <h1>Hello Java</h1>
+                    </body>
+                </html>
