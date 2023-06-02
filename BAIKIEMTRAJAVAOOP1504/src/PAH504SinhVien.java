d36c136520929171ba15b1a5164984d370822f56
hoang2k3ntnd <126093207+hoang2k3ntnd@users.noreply.github.com>
2023-06-02 10:03:58 +0700

    Add files via upload

---
+++ BAIKIEMTRAJAVAOOP1504/src/PAH504SinhVien.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public abstract class PAH504SinhVien{
+    private static final String ArrayList = null;
+    static int HoVaTen;
+    static int Tuoi;
+    static String Diachi;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        public Void NhapThongTin(ArrayList<PAH504SinhVien> ArrayList, int n){
+            Scanner sc = new Scanner(system.in);
+            for(i=0, i<n, i++){
+        Ten = sc.nextInt();
+        System.out.println("Nhap ten cua sinh vien :"+i);
+        Tuoi = sc.nextInt();
+        System.out.println("Nhap tuoi cua sinh vien :"+i);
+        Diachi = sc.nextInt();
+        System.out.println("Nhap dia chi cua sinh vien :"+i);
+            Student.student = new student();
+            Student.HoVaTen = HoVaTen ;
+            Student.Tuoi = Tuoi;
+            Student.Diachi = Diachi;
+            ArrayList.add(student)
+
+            }
+            
+        }
+    public void ThemThongTin(ArrayList<PAH504SinhVien>Arraylist, int n){
+        Scanner sc = new Scanner(system.in);
+        
+        }
+        
+
+    }
+
+
+        
+    }
+
